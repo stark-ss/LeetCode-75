ac74f4f82c4f38c5501e71df89d16ba2d71bdfa2
stark-ss <samantasoumyadeep123@gmail.com>
2026-06-23 00:32:15 +0530

    Time: 7 ms (87.94%), Space: 29.1 MB (82.5%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-            int n=grid.size();
+    int n=grid.size();
     vector<vector<int>> k;
     int c=0;
+    map<vector<int>,int> row;
+    for(int i=0;i<n;i++)
+    row[grid[i]]++;
     vector<int> col;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
         col.push_back(grid[j][i]);
       }
-      k.push_back(col);
+      if(row.find(col)!=row.end())
+      c+=row[col];
       col.clear();
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-         if(k[i]==grid[j])
-         c++;
-        }
     }
 return c;
     }
